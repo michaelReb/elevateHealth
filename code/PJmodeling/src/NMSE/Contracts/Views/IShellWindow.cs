90a9dcd655fedd5bb8142df9f8228ba9d58b41a7
Michael Rebhan <51949399+michaelReb@users.noreply.github.com>
2024-12-12 16:08:20 +0100

    Add files via upload

---
+++ code/PJmodeling/src/NMSE/Contracts/Views/IShellWindow.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace NMSE.Contracts.Views;
+
+public interface IShellWindow
+{
+    Frame GetNavigationFrame();
+
+    void ShowWindow();
+
+    void CloseWindow();
+}
